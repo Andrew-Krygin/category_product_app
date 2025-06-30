d550cc85108ef3bf58b217632eeabf69a53209f0
Andrew Krygin <andrew_krygin@protonmail.com>
2025-06-30 14:06:13 +0300

    Update data_for_models module
    - Refactor list_product: replace dictionaries with Product class objects
    - Add valid_case_new_product and invalid_case_new_product

--- tests/tests_data/data_for_models.py
+++ tests/tests_data/data_for_models.py
@@ -1,10 +1,31 @@
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
+from models.product import Product
+
+# Список продуктов для класса Category
 list_products = [
+    Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+    Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+    Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+]
+
+# Корректный продукт для тестирования класса Product.
+valid_case_new_product = (
     {
         "name": "Samsung Galaxy C23 Ultra",
         "description": "256GB, Серый цвет, 200MP камера",
         "price": 180000.0,
         "quantity": 5,
     },
-    {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
-    {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
-]
+    Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+    does_not_raise(),
+)
+
+# Некорректный продукт для тестирования класса Product.
+invalid_case_new_product = (
+    {"name": "Samsung Galaxy C23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0},
+    None,
+    pytest.raises(ValueError, match="A key required to create an object of " "class Product is missing."),
+)
