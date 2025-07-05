3b8e86fc78fcb812cffb2511520a38efe03b0c60
Andrew Krygin <andrew_krygin@protonmail.com>
2025-07-05 13:30:06 +0300

    conftest: add two fixtures for tests
    - sample_smartphone: returns a Smartphone instance
    - sample_lawn_grass: returns a LawnGrass instance

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import copy
 import pytest
 
 from models.category import Category
-from models.product import Product
+from models.product import LawnGrass, Product, Smartphone
 from tests.tests_data.data_for_models import list_products
 
 
@@ -20,3 +20,15 @@ def sample_category() -> Category:
 @pytest.fixture
 def sample_products() -> Product:
     return Product("Iphone 15", "512GB, Gray space", 31000.0, 14)
+
+
+@pytest.fixture
+def sample_smartphone() -> Smartphone:
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
+    )
+
+
+@pytest.fixture
+def sample_lawn_grass() -> LawnGrass:
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
